aabb240eefef848113b0440132fef64b99d02134
jiaxing <jiaxing.yee@marinabaysands.com>
2021-03-22 17:54:58 +0800

    added basic DI codes

---
+++ app/src/main/java/com/jx/daggerpractice/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.jx.daggerpractice;
+
+import com.jx.daggerpractice.di.component.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
