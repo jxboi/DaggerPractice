baf00b24fd20a1fb4e9c2d0a4be58bdd1a69a42c
jiaxing <jiaxing.yee@marinabaysands.com>
2021-03-22 21:17:00 +0800

    new module

--- app/src/main/java/com/jx/daggerpractice/di/component/AppComponent.java
+++ app/src/main/java/com/jx/daggerpractice/di/component/AppComponent.java
@@ -4,6 +4,7 @@ package com.jx.daggerpractice.di.component;
 import android.app.Application;
 
 import com.jx.daggerpractice.BaseApplication;
+import com.jx.daggerpractice.di.module.ActivityBuildersModule;
 
 import dagger.BindsInstance;
 import dagger.Component;
@@ -13,6 +14,8 @@ import dagger.android.support.AndroidSupportInjectionModule;
 @Component(
         modules = {
                 AndroidSupportInjectionModule.class,
+                ActivityBuildersModule.class,
+
         }
 )
 public interface AppComponent extends AndroidInjector<BaseApplication> {
